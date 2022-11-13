ebe123abb6275958e6370b66e25194a356c1f83b
Aravind601 <aravindasamy8601@gmail.com>
2022-11-13 14:36:37 +0530

    login and signup form

--- Final Deliverables/fnalcode-ckd/app.py
+++ Final Deliverables/fnalcode-ckd/app.py
@@ -35,3 +35,30 @@ def help():
 @app.route('/terms')
 def terms():
     return render_template("tc.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
+
+        return render_template("login.html", form=form)
+    return render_template("login.html", form=form)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect("/login")
+    return render_template('signup.html', form=form)
