5682954d259c22b37893b5516e90a278165e6e26
Aravind601 <aravindasamy8601@gmail.com>
2022-11-13 14:13:39 +0530

    install dependencies

---
+++ Final Deliverables/fnalcode-ckd/app.py
@@ -0,0 +1,14 @@
+import joblib
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+import pandas as pd
+import pickle
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
